36e47b100a5980e8a9677ecb3aea0be4bc60b1be
yu1hpa <yu.1hpa@gmail.com>
2023-01-21 02:32:54 +0900

    feat(command): Add the ability to register user info in the DB

--- bot/src/bot.ts
+++ bot/src/bot.ts
@@ -1,8 +1,7 @@
-import {
-    Client,
-    GatewayIntentBits
-} from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
+import getDataSource from "./config";
+import { Member } from "./entities/member";
 
 dotenv.config();
 
@@ -19,6 +18,27 @@ client.on("messageCreate", async (m) => {
     const pong = await m.channel.send("Pong");
     console.log(pong.content);
   }
+
+  if (m.content === "!register") {
+    const dataSource = await getDataSource();
+    const m_author_id = parseInt(m.author.id);
+    await dataSource.getRepository(Member).findOneBy({
+      user_id: m_author_id,
+    });
+    await dataSource
+      .createQueryBuilder()
+      .insert()
+      .into(Member)
+      .values({
+        user_id: m_author_id,
+        username: m.author.username ?? "",
+        user_level: 0,
+        points_sum: 0,
+      })
+      .execute();
+    const p = await m.channel.send(m.author.id);
+    console.log(p.content);
+  }
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN);
